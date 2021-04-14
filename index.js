b365156bf1e3a3a1b594c9540927f3efc4a184f5
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-04-14 18:23:19 -0400

    refractored code added function for fetch

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const li = document.createElement('li')
 const ul = document.getElementById('users')
 
 
+function getUsers() {
 fetch("http://127.0.0.1:3000/users")
 .then(response => response.json())
 .then(renderUsers)
+} 
 
 function renderUsers(json) {
-  json.map(e => {
-    li.innerText = e.name
-    ul.appendChild(li)
+  json.map(user => {
+    renderUser(user)
   })
 }
 
+function renderUser(user) {
+  const li = document.createElement('li')
+  li.innerText = user.name
+  ul.appendChild(li)
+}
+
+getUsers()
