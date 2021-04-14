d9c5f78ddb9e319968c092b7c2d1ffc82be3376f
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-04-14 19:18:04 -0400

    added event listener for form/fetch to handle create

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+const baseURL = "http://127.0.0.1:3000"
+const usersURL = baseURL + '/users'
+
+
 const ul = document.getElementById('users')
 const form = document.getElementById('user-form')
 
 function getUsers() {
-fetch("http://127.0.0.1:3000/users")
+fetch(usersURL)
 .then(response => response.json())
 .then(renderUsers)
 } 
+
+form.addEventListener('submit', handleFormSubmit)
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+  const userData = {
+    name: e.target.user.value
+  }
+
+  const configObj = {
+    method: 'POST', 
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(userData)
+  }
+
+  fetch(usersURL, configObj)
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}
 
 function renderUsers(json) {
   json.map(user => {
