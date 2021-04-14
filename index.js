542ddd09222591dba5737e03e087e263efd3ce8b
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-04-14 17:53:29 -0400

    refractored fetch for users

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const li = document.createElement('li')
+const ul = document.getElementById('users')
+
+
 fetch("http://127.0.0.1:3000/users")
 .then(response => response.json())
-.then(data => {
-  const li = document.createElement('li')
-  const ul = document.getElementById('users')
-  data.map(e => {
+.then(renderUsers)
+
+function renderUsers(json) {
+  json.map(e => {
     li.innerText = e.name
     ul.appendChild(li)
   })
-})
+}
 
