c75771c7cae0522bb9e826ab5c714eb13a82a6b3
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-04-18 10:47:21 -0400

    added to UserApi

--- index.js
+++ index.js
@@ -1,51 +1,13 @@
-// const baseURL = "http://127.0.0.1:3000"
-// const usersURL = baseURL + '/users'
-
-
 const ul = document.getElementById('users')
 const form = document.getElementById('user-form')
 
-
-// function getUsers() {
-// fetch(usersURL)
-// .then(response => response.json())
-// .then(renderUsers)
-// } 
 
 form.addEventListener('submit', handleFormSubmit)
 
 function handleFormSubmit(e) {
   e.preventDefault()
-  const userData = {
-    name: e.target.user.value
-  }
-
-  const configObj = {
-    method: 'POST', 
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify(userData)
-  }
-
-  fetch("http://127.0.0.1:3000/users", configObj)
-  .then(resp => resp.json())
-  .then(user => {
-    const i = new User({name: user.name})
-    debugger
-    i.attachToDom()
-    form.reset()
-  })
+  UserApi.createUser(e)
+  form.reset()
 }
-
-// function renderUsers(json) {
-//   json.map(user => {
-//     const i = new User({name: user.name})
-//     i.attachToDom()
-//   })
-
-
-
 
 UserApi.getUsers()
--- userApi.js
+++ userApi.js
@@ -1,5 +1,3 @@
-
-
 class UserApi {
 
   static baseURL = "http://127.0.0.1:3000/users"
@@ -13,7 +11,29 @@ class UserApi {
         i.attachToDom()
       })
     })
-    } 
+  }
+  
+  static createUser(e) {
+    const userData = {
+      name: e.target.user.value
+    }
+  
+    const configObj = {
+      method: 'POST', 
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(userData)
+    }
+  
+    fetch(this.baseURL, configObj)
+    .then(resp => resp.json())
+    .then(user => {
+      const i = new User({name: user.name})
+      i.attachToDom()
+    })
+  }
 
 }
 
