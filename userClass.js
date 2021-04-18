d48649c124bd6f4dadd2f488dd3ad9af3d4116f9
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-04-17 20:58:06 -0400

    adding functions to user class

--- userClass.js
+++ userClass.js
@@ -2,30 +2,36 @@ class User {
 
   static all = []
 
-  constructor({id, name}) {
+  constructor({name}) {
     this.name = name 
-    this.id = id
+
     
     this.element = document.createElement('li')
-    this.element.id = `user-${id}`
+
 
     User.all.push(this)
   }
 
-  renderUser(li) {
+  renderUser() {
     this.element.innerHTML = `
-      <div user-id="${this.id}"> 
+      <div> 
       <strong class="name">${this.name}</strong>
       </div> 
     `
+
+    return this.element
+  }
+
+  attachToDom() {
+    ul.appendChild(li)
   }
 
 
 }
 
-function renderUser(user) {
-  const li = document.createElement('li')
-  li.id = `user ${user.id}`
-  li.innerText = user.name
-  ul.appendChild(li)
-}
+// function renderUser(user) {
+//   const li = document.createElement('li')
+//   li.id = `user ${user.id}`
+//   li.innerText = user.name
+//   ul.appendChild(li)
+// }
