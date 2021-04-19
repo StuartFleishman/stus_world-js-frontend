23d07bfe070c510af3cdce6483d3c3d9631b9b9f
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-04-19 09:16:21 -0400

    added dice function

--- app.js
+++ app.js
@@ -35,6 +35,18 @@
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
   ]
 
+  function rollDice() {
+    const die1 = document.getElementById('die1')
+    const die2 = document.getElementById('die2')
+    const status = document.getElementById('status')
+    const d1 = Math.floor(Math.random() * 6) + 1;
+    const d2 = Math.floor(Math.random() * 6) + 1;
+    const diceTotal = d1 + d2;
+    die1.innerHTML = d1
+    die2.innerHTML = d2
+    status.innerHTML = diceTotal
+  }
+
 
   const squares = []
 
