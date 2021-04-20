97d577aa76fcee5bf90f15f37ba92981faa7351e
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-04-20 13:41:12 -0400

    making racket for game

--- game.js
+++ game.js
@@ -2,5 +2,10 @@ let canvas = document.getElementById('gameScreen')
 
 let context = canvas.getContext('2d')
 
+context.clearRect(0,0, 800, 600)
+
+const gWidth = 800;
+const gHeight = 600;
+
 context.fillStyle = '#00f';
 context.fillRect(20, 20, 100, 100);
---
+++ racketClass.js
@@ -0,0 +1,15 @@
+class Racket {
+  constructor(gwidth, gHeight) {
+    this.width = 150; 
+    this.height = 30;
+
+    this.position = {
+      x: 1,
+      y: 1,
+    }
+  }
+
+  draw(context) {
+
+  }
+}
